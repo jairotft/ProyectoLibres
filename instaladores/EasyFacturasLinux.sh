c6da5b3f1320a4e1945f868ce350a32746443860
mathcrap <mathcrap@localhost.localdomain>
2017-06-12 13:47:22 -0500

    Instalador base

---
+++ instaladores/EasyFacturasLinux.sh
@@ -0,0 +1,10 @@
+cd ~
+mkdir -p .easyfacturas
+export user=$USER
+curl -LSso ~/.easyfacturas/jar.zip https://github.com/Jairo95/ProyectoLibres/raw/instalacion/instaladores/jar.zip
+cd .easyfacturas
+unzip jar.zip
+echo "java -jar ./jar/AplicacionesLibres.jar" > EasyFacturas.bin
+sudo cp jar/settings/EasyFacturas.desktop /usr/share/applications/
+sudo cp jar/settings/EasyFacturas.ico /usr/share/icons/
+rm jar.zip

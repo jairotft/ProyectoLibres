41f67b0e9e2dbbf84814e4be2de02b5a322be88c
Oscar <oscar-crs@hotmail.es>
2017-07-22 12:18:57 -0500

    cambio fecha interfaz de bienvenida

--- AppLibres/src/Interfaces/Bienvenida.java
+++ AppLibres/src/Interfaces/Bienvenida.java
@@ -52,7 +52,7 @@ public class Bienvenida extends javax.swing.JFrame {
 
         jLabel5.setText("PROYECTO DE ASIGNATURA: CAPTURADOR DE FACTURAS XML");
 
-        jLabel6.setText("FECHA:27/06/2017");
+        jLabel6.setText("FECHA:25/07/2017");
 
         btnContinuar.setText("CONTINUAR");
         btnContinuar.addActionListener(new java.awt.event.ActionListener() {

1179f27f45e07c7d9b344ceab510b3a3e9458610
root <root@localhost.localdomain>
2017-08-07 16:17:32 -0500

    Clase completada, lee el 95% de las facturas ingresadas, excepto aquellas que no son declaradas de acuerdo al estandar xml

--- AplicacionesLibres/src/main/java/com/libres/aplicacioneslibres/aplicacioneslibres/CargaXml.java
+++ AplicacionesLibres/src/main/java/com/libres/aplicacioneslibres/aplicacioneslibres/CargaXml.java
@@ -26,6 +26,7 @@ import org.jdom2.input.SAXBuilder;
 
 public class CargaXml {
     
+    @SuppressWarnings("null")
     public void cargarXml(String name, String cedulaCli, int anio, String tipo) {
         //Se crea un SAXBuilder para poder parsear el archivo
         SAXBuilder builder = new SAXBuilder();
@@ -37,105 +38,90 @@ public class CargaXml {
             
             document = (Document) builder.build(xmlFile);
             Conexion cp = new Conexion();
-            
-        //Info Producto
-            
+            //Hashmap para extraer datos de documento XML
             HashMap<String,String> infoEncabezado = new HashMap<>();
-            infoEncabezado.put("estado", "");
-            infoEncabezado.put("ambiente", "");
-            
             HashMap<String,String> infoTributaria = new HashMap<>();
-            infoTributaria.put("razonSocial", "");
-            infoTributaria.put("ruc", "");
-            infoTributaria.put("estab", "");
-            infoTributaria.put("ptoEmi", "");
-            infoTributaria.put("secuencial", "");
-            infoTributaria.put("dirMatriz", "");
-            
             HashMap<String,String> infoFactura = new HashMap<>();
-            infoFactura.put("fechaEmision", "");
-            infoFactura.put("razonSocialComprador", "");//nombre_Comprador
-            infoFactura.put("identificacionComprador", "");//cedula_comprador
-            infoFactura.put("fechaEmision", "");
-            infoFactura.put("totalSinImpuestos", "");
-            infoFactura.put("valor", "");//valor de impuestos
-            infoFactura.put("importeTotal", "");//Valor final de esa factura,con propina o descuento 
-
-            //informacion de un producto
-            HashMap<String,String> infoDetalle = new HashMap<>();
-            infoDetalle.put("codigoPrincipal", "");//Codigo
-            infoDetalle.put("descripcion", "");//Nombre
-            infoDetalle.put("cantidad", "");//cantidad
-            infoDetalle.put("precioUnitario", "");//precio unitario
-            infoDetalle.put("precioTotalSinImpuesto", "");//Precio total sin iva del producto
-
-            //informacion de la lista de productos
+            //informacion de la lista de productos 
+            //guardar cada detalle( o info de Producto) en la lista infoDetalles
             ArrayList<HashMap> infoDetalles = new ArrayList<>();
             
+                              
+            Element rootNode; //Se obtiene la raiz 'tables'
+            Element rootAutorizacion;//nodo raiz autorizacion
+            Element rootComprobante;//nodo raiz Comprobante
+            //Comprobante tiene 3 hijos, infoTributaria, infoFactura y infoDetalles
+            Element rootInfoTributaria;//nodo raiz InfoTributaria
+            Element rootInfoFactura;//nodo raiz infoFactura
+            Element rootInfoDetalles;//nodo raiz infoDetalles
+            Element rootInfoDetalle;//nodo raiz infoDetalle
             
-            //Se obtiene la raiz 'tables'
-            Element rootNode = document.getRootElement(); //Autorizacion
-
+             rootNode= document.getRootElement(); //Nodo raiz del documento XML
+            //Encontrar el nodo autorizacion
+                rootAutorizacion = encontrarNodo(rootNode, "autorizacion");
+                
             // Datos Cabecera =============================================
-            infoEncabezado.replace("estado", rootNode.getChild("estado").getTextTrim());
-            infoEncabezado.replace("ambiente", rootNode.getChild("ambiente").getTextTrim());
+            infoEncabezado.put("estado", nodoGetValor(rootAutorizacion,"estado"));
+            infoEncabezado.put("ambiente", nodoGetValor(rootAutorizacion,"ambiente"));
             
             // Datos Comprobante =======================================
-            Element tabla = rootNode.getChild("comprobante");
             
-            if (tabla != null) {
-                String ex = tabla.getText();
+                //Si comprobante es hermano de autorizacion, entonces tomar rootNodo
+                rootComprobante = encontrarNodo(rootNode,"comprobante");
+                //Si no existe, buscar comprobante como hijo de rootAutorizacion
+                if(rootComprobante==null)
+                    rootComprobante = encontrarNodo(rootAutorizacion,"comprobante");
+        
+                //Lectura CDATA
+            if (rootComprobante != null) {
+                String ex = rootComprobante.getText();
                 InputStream stream = new ByteArrayInputStream(ex.getBytes("UTF-8"));
                 Document parse = builder.build(stream);
-                tabla = parse.getRootElement();
+                rootComprobante = parse.getRootElement();//Obtener 
             } else {
-                tabla = rootNode;
+                //Hay facturas sin cabecera autorizacions
+                rootComprobante = rootNode;
             }
             
             //Comprobante tiene 3 hijos, Info Tributaria, Info Factura y Detalles
-            List lista_campos = tabla.getChildren();//guardo en una lista los 3 hijos
-            Element campo;
 
-            Element tributaria = (Element) lista_campos.get(0);
-
-            // Info Tributaria
-            infoTributaria.replace("razonSocial", tributaria.getChildTextTrim("razonSocial"));
-            infoTributaria.replace("dirMatriz", tributaria.getChildTextTrim("dirMatriz"));
-            infoTributaria.replace("ruc", tributaria.getChildTextTrim("ruc"));
-            infoTributaria.replace("estab", tributaria.getChildTextTrim("estab"));
-            infoTributaria.replace("ptoEmi", tributaria.getChildTextTrim("ptoEmi"));
-            infoTributaria.replace("secuencial", tributaria.getChildTextTrim("secuencial"));
+            // Info Tributaria=======================================================================
+            rootInfoTributaria = (Element) encontrarNodo(rootComprobante, "infoTributaria");
+            //Agregamos la Clave y su Valor de cada dato al hashmap infoTributaria
+            infoTributaria.put("razonSocial", nodoGetValor(rootInfoTributaria,"razonSocial"));
+            infoTributaria.put("dirMatriz", nodoGetValor(rootInfoTributaria,"dirMatriz"));
+            infoTributaria.put("ruc", nodoGetValor(rootInfoTributaria,"ruc"));
+            infoTributaria.put("estab", nodoGetValor(rootInfoTributaria,"estab"));
+            infoTributaria.put("ptoEmi", nodoGetValor(rootInfoTributaria,"ptoEmi"));
+            infoTributaria.put("secuencial", nodoGetValor(rootInfoTributaria,"secuencial"));
             
-            //Info Factura
-            Element factura = (Element) lista_campos.get(1);
+            //Info Factura==========================================================================
+            rootInfoFactura = (Element) encontrarNodo(rootComprobante, "infoFactura");
             
-            infoFactura.replace("fechaEmision", factura.getChildTextTrim("fechaEmision"));  
-            infoFactura.replace("razonSocialComprador", factura.getChildTextTrim("razonSocialComprador"));  
-            infoFactura.replace("identificacionComprador", factura.getChildTextTrim("identificacionComprador"));  
-            infoFactura.replace("totalSinImpuestos", factura.getChildTextTrim("totalSinImpuestos"));    
-                
-                //TotalConImpuestos tiene dos hijos, el segundo campo (.getChild(1) )tiene el valor $ IVA
-                List totalConImp = factura.getChild("totalConImpuestos").getChildren();
-                Element totalImp = (Element) totalConImp.get(1);
-                infoFactura.replace("valor", totalImp.getChildTextTrim("valor"));    
+            //GUARDAR FECHA EMISION EN EL FORMATO YY-MM-DD
             
-            //Total a pagar 
-            infoFactura.replace("importeTotal", tributaria.getChildTextTrim("importeTotal"));
+            infoFactura.put("fechaEmision", miFormatoYYMMDD( nodoGetValor(rootInfoFactura,"fechaEmision")) );  
+            infoFactura.put("razonSocialComprador", nodoGetValor(rootInfoFactura,"razonSocialComprador"));  
+            infoFactura.put("identificacionComprador", nodoGetValor(rootInfoFactura,"identificacionComprador"));  
+            infoFactura.put("totalSinImpuestos", nodoGetValor(rootInfoFactura,"totalSinImpuestos")); 
+            //TOTAL A PAGAR CON IMPUESTOS
+            infoFactura.put("importeTotal", nodoGetValor(rootInfoFactura,"importeTotal"));
             
-            //Info Detalles
-            Element detalles = (Element) lista_campos.get(2);   //Detalles...
-            List detalle = detalles.getChildren();  //extraido los hijos(detalle) en una lista auxiliar
+            //Info Detalles=============================================================================
+            rootInfoDetalles = (Element) encontrarNodo(rootComprobante, "detalles");   //Detalles...
+            List detalle = rootInfoDetalles.getChildren();  //extraido los hijos(detalle) en una lista auxiliar
             
             for (int j = 0; j < detalle.size(); j++) {
-                campo = (Element) detalle.get(j);
-                // Detalle    
+                rootInfoDetalle = (Element) detalle.get(j);
+                // Detalle o informacion de Producto
+                HashMap<String,String> infoDetalle=new HashMap<>();
                 //Descripcion= Nombre del producto
-                infoDetalle.replace("codigoPrincipal", campo.getChildTextTrim("codigoPrincipal"));
-                infoDetalle.replace("descripcion", campo.getChildTextTrim("descripcion"));
-                infoDetalle.replace("cantidad", campo.getChildTextTrim("cantidad"));
-                infoDetalle.replace("precioUnitario", campo.getChildTextTrim("precioUnitario"));
+                infoDetalle.put("codigoPrincipal", nodoGetValor(rootInfoDetalle,"codigoPrincipal"));
+                infoDetalle.put("descripcion", nodoGetValor(rootInfoDetalle,"descripcion"));
+                infoDetalle.put("cantidad", nodoGetValor(rootInfoDetalle,"cantidad"));
+                infoDetalle.put("precioUnitario", nodoGetValor(rootInfoDetalle,"precioUnitario"));
                 //PrecioTotalSinImpuesto = PRECIO_UNITARIO * UNIDADES
-                infoDetalle.replace("precioTotalSinImpuesto", campo.getChildTextTrim("precioTotalSinImpuesto"));
+                infoDetalle.put("precioTotalSinImpuesto", nodoGetValor(rootInfoDetalle,"precioTotalSinImpuesto"));
                 
                 //agregamos el detalle a la lista de detalles SSI el Nombre_producto != Empty
                 if (!infoDetalle.get("descripcion").equals("")) {
@@ -143,46 +129,34 @@ public class CargaXml {
                 }    
             }
             
-            // Extraer Anio de fecha de Emision 02-05-2016 02/04/2016 2016/04/26 2017-05-05
-            String fechaEmision = infoFactura.get("fechaEmision");            
-            //convierte la fecha DD/MM/YY a DD-MM-YY y la guarda en un arreglo
-            String[] fecha = fechaEmision.replace('/', '-').split("-");
-            String DIA=fecha[0];
-            String MES=fecha[1];
-            String ANIO=fecha[2];
-            //Pero si la fechaEmision es de la forma 2017-08-05
-            if(fecha[0].length()==4) {
-                ANIO=fecha[0];
-                DIA=fecha[2];
-            }
-            //GUARDAR FECHA EMISION EN EL FORMATO YY-MM-DD
-            infoFactura.replace("fechaEmision", ANIO+"-"+MES+"-"+DIA);
-            
-            //sentencia para validar si la factura pertenece a otro usuario
-            String sqlUser="select *from Cliente where id_cliente='"+infoFactura.get("identificacionComprador")+"'";
-            
             //El numero de factura es : estab-ptoEmi-secuencial
             //numFact para validar si la factura ya ha sido ingresada
             String numFact = infoTributaria.get("estab") + "-" 
                     + infoTributaria.get("ptoEmi") + "-" 
                     + infoTributaria.get("secuencial");
             
+            //            JOptionPane.showMessageDialog(null, infoEncabezado.values());
+            //            JOptionPane.showMessageDialog(null, infoTributaria.values());
+            //            JOptionPane.showMessageDialog(null, infoFactura.values());
+            //            JOptionPane.showMessageDialog(null, infoDetalles.toString());
+            
             //VALIDACIONES
             String opcion="";
-            
-            if( !ANIO.equals(anio) ) { //si el Anio es deferente!! 
+            String ANIO=infoFactura.get("fechaEmision");
+            if( ANIO.compareTo(anio+"") < 0 ) { //-1 si el Anio es diferente!! 
                 opcion="problemaAnio";
                 
-            }else if( !cp.verificar_usuario(sqlUser) ){ //si el usuario es diferente!!
+            }else if( cedulaCli.equals(infoFactura.get("identificacionComprador") )==false){
+            //si la cedula del usuario no es igual a la de la factura que quiere ingresar
                 opcion="facturaDeOtroUsuario";
                 
             }else if( cp.verificar_factura(numFact) ){ //si la factura ya fue ingresada!!
                 opcion="facturaYaIngresada";
                 
-            }else if( tipo.equals("Personal") ) { 
+            }else if( tipo.equals("Personal") ==true ) { 
                 opcion="interfazPersonal";
                 
-            }else if( tipo.equals("Negocio") ) {
+            }else if( tipo.equals("Negocio") == true ) {
                 opcion="interfazNegocio";
                 
             }
@@ -197,7 +171,6 @@ public class CargaXml {
                     
                 case "facturaDeOtroUsuario":
                     JOptionPane.showMessageDialog(null, "Esta factura pertenece a otro usuario");
-                    
                     break;
                     
                 case "facturaYaIngresada":
@@ -205,19 +178,73 @@ public class CargaXml {
                     break;
                    
                 case "interfazPersonal":
-                    SeleccionarTipoGastoPersonal sP = new SeleccionarTipoGastoPersonal( infoEncabezado, infoTributaria, infoDetalles);
+                    SeleccionarTipoGastoPersonal sP = new SeleccionarTipoGastoPersonal( infoEncabezado, infoTributaria,infoFactura, infoDetalles);
                     sP.setVisible(true);
                 
                     break;
                 case "interfazNegocio":
-                    SeleccionarTipoGastoNegocios sN = new SeleccionarTipoGastoNegocios(infoEncabezado, infoTributaria, infoDetalles);
+                    SeleccionarTipoGastoNegocios sN = new SeleccionarTipoGastoNegocios(infoEncabezado, infoTributaria,infoFactura, infoDetalles);
                     sN.setVisible(true);
                     break;
             }
                 
-                
             } catch (JDOMException | IOException ex) {
                 Logger.getLogger(CargaXml.class.getName()).log(Level.SEVERE, null, ex);
             }
-    }   
+    }
+    
+    //Metodo recursivo para encontrar un nodo del Documento XML
+    public Element encontrarNodo(Element padre,String nombreNodo){
+        //caso base si lo encuentra return Padre
+        if(padre ==null) return null;
+        if(padre.getName().equals(nombreNodo)){
+            //JOptionPane.showMessageDialog(null, "padre "+padre);
+            return padre;
+        }else{
+            //Si no lo encuentra, se genera una lista de sus hijos
+            List hijos = padre.getChildren();
+            
+            Element hijo = null;//El hijo debe inicializarse
+            for (int i=0; i<hijos.size();i++){//recorre los hijos de la lista
+                hijo = (Element)hijos.get(i);//extrae hijo en la posicion i
+                if(hijo!=null){
+                    if(hijo.getName().equals(nombreNodo)){
+                    //JOptionPane.showMessageDialog(null, "hijo "+hijo);
+                    break;
+                    }
+                }
+            }
+            return encontrarNodo(hijo, nombreNodo);
+        }       
+    }
+    
+    //Metodo que devuele el valor de un atributo XML
+    //Si no existe el atributo retorna ""
+    public String nodoGetValor(Element padre,String nombreNodo){
+        Element element = encontrarNodo(padre, nombreNodo);
+        //JOptionPane.showMessageDialog(null, element==null);
+        if(element==null){
+            return " ";
+        }else{
+            //JOptionPane.showMessageDialog(null,element.getTextTrim());
+            return element.getTextTrim();
+        }
+    }
+    
+     // Convertir fecha de Emision 02-05-2016 02/04/2016 2016/04/26 => 2017-05-05
+    public String miFormatoYYMMDD(String fechaEmision){
+        
+        //convierte la fecha DD/MM/YY a DD-MM-YY y la guarda en un arreglo
+        String[] fecha = fechaEmision.replace('/', '-').split("-");
+        String DIA=fecha[0];
+        String MES=fecha[1];
+        String ANIO=fecha[2];
+        //Pero si la fechaEmision es de la forma 2017-08-05
+        if(fecha[0].length()==4) {
+            ANIO=fecha[0];
+            DIA=fecha[2];
+        }
+        return ANIO +"-"+ MES +"-"+ DIA;
+}
+    
 }
